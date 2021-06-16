da92ce4f8c6e6b149345e0689157364c9b234a04
Erkan Yıldırım <erkanyildirim28@gmail.com>
2021-06-16 09:15:15 +0300

    brand

--- src/layouts/Navi.js
+++ src/layouts/Navi.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import {Nav, Navbar, NavDropdown} from "react-bootstrap";
+import {Nav, Navbar, NavDropdown, Container} from "react-bootstrap";
 
 export default function Navi() {
     return (
         <div>
-            <Navbar bg="light" expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand href="#home">Hrms</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
+            <Container>
+                <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                    <Navbar.Brand href="#home">Hrms</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#features">Features</Nav.Link>
+                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+
                     <Nav className="mr-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                        <Nav.Link href="#deets">More deets</Nav.Link>
+                        <Nav.Link eventKey={2} href="#memes">
+                            Dank memes
+                        </Nav.Link>
+                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
+                            <NavDropdown.Divider/>
                             <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
-                </Navbar.Collapse>
-            </Navbar>
+                </Navbar>
+            </Container>
         </div>
     );
 }
--- src/pages/JobAdvertList.js
+++ src/pages/JobAdvertList.js
@@ -1,42 +1,45 @@
-import React, {useState,useEffect} from 'react'
+import React, {useState, useEffect} from 'react'
 import JobAdvertService from "../services/JobAdvertService";
 import {Table} from "reactstrap";
+import {Container} from "react-bootstrap";
 
 function JobAdvertList(props) {
 
     const [jobAdverts, setJobAdverts] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         let jobAdvertService = new JobAdvertService();
-        jobAdvertService.getJobAdverts().then(result=>setJobAdverts(result.data.data));
-    },[])
+        jobAdvertService.getJobAdverts().then(result => setJobAdverts(result.data.data));
+    }, [])
 
     return (
         <div>
-            <Table striped bordered hover size="sm" className="mt-sm-5">
-                <thead>
-                <tr>
-                    <th>Firm Name</th>
-                    <th>Position Name</th>
-                    <th>Open Positions Number</th>
-                    <th>Create Date</th>
-                    <th>Apply Deadline</th>
-                </tr>
-                </thead>
-                <tbody>
-                {
-                    jobAdverts.map(jobAdvert => (
-                        <tr key={jobAdvert.firmName}>
-                            <td>{jobAdvert.firmName}</td>
-                            <td>{jobAdvert.positionName}</td>
-                            <td>{jobAdvert.openPositionsNumber}</td>
-                            <td>{jobAdvert.createDate}</td>
-                            <td>{jobAdvert.applyDeadline}</td>
-                        </tr>
-                    ))
-                }
-                </tbody>
-            </Table>
+            <Container>
+                <Table striped bordered hover size="sm" className="mt-sm-5">
+                    <thead>
+                    <tr>
+                        <th>Firm Name</th>
+                        <th>Position Name</th>
+                        <th>Open Positions Number</th>
+                        <th>Create Date</th>
+                        <th>Apply Deadline</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        jobAdverts.map(jobAdvert => (
+                            <tr key={jobAdvert.firmName}>
+                                <td>{jobAdvert.firmName}</td>
+                                <td>{jobAdvert.positionName}</td>
+                                <td>{jobAdvert.openPositionsNumber}</td>
+                                <td>{jobAdvert.createDate}</td>
+                                <td>{jobAdvert.applyDeadline}</td>
+                            </tr>
+                        ))
+                    }
+                    </tbody>
+                </Table>
+            </Container>
         </div>
     );
 }
