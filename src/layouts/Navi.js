bad3c83aeb5df2db842da69fe088353890496fc9
Erkan Yıldırım <erkanyildirim28@gmail.com>
2021-06-16 08:22:52 +0300

    brand

--- src/layouts/Navi.js
+++ src/layouts/Navi.js
@@ -5,7 +5,7 @@ export default function Navi() {
     return (
         <div>
             <Navbar bg="light" expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home">Hrms</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
