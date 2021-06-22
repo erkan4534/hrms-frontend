f33424d6143aede37fac34147aa0cc6681c024e2
Erkan Yıldırım <erkanyildirim28@gmail.com>
2021-06-23 00:57:18 +0300

    add router

--- src/layouts/Dashboard.js
+++ src/layouts/Dashboard.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import {Col, Container, Row} from "react-bootstrap";
+import {Col, Row} from "react-bootstrap";
 import Side from "./Side";
 import JobAdvertList from "../pages/JobAdvertList";
+import {Route,Switch} from "react-router-dom";
 
 function Dashboard() {
     return (
         <div>
-
                 <Row >
                     <Col  sm={2} ><Side/></Col>
                     <Col  sm={10}>
-                        <JobAdvertList/>
+                        <Route exact path="/" component={JobAdvertList}/>
+                        <Route exact path="/adverts" component={JobAdvertList}/>
                     </Col>
                 </Row>
 
