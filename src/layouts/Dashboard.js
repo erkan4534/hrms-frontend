04e2e1a911306cd79ea7b5afd8479dcdc3064336
Erkan Yıldırım <erkanyildirim28@gmail.com>
2021-07-03 00:54:05 +0300

    toolbar color

--- src/layouts/Dashboard.js
+++ src/layouts/Dashboard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
@@ -14,9 +14,9 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import { mainListItems, secondaryListItems } from './ListItems';
+import {mainListItems, secondaryListItems} from './ListItems';
 import JobAdvertList from "../pages/JobAdvertList";
-import {Row,Col} from "react-bootstrap";
+import {Row, Col} from "react-bootstrap";
 import {Route} from "react-router-dom";
 import {ToastContainer} from "react-toastify";
 import Employer from "../pages/Employer";
@@ -114,16 +114,16 @@ export default function Dashboard() {
     return (
         <div className={classes.root}>
             <ToastContainer position="bottom-right"/>
-            <CssBaseline />
+            <CssBaseline/>
             <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
-                <Toolbar className={classes.toolbar} style={{backgroundColor:"#007bff"}}>
+                <Toolbar className={classes.toolbar} style={{backgroundColor: "#007bff"}}>
                     <IconButton
                         edge="start"
                         color="inherit"
                         aria-label="open drawer"
                         onClick={handleDrawerOpen}
                         className={clsx(classes.menuButton, open && classes.menuButtonHidden)}>
-                        <MenuIcon />
+                        <MenuIcon/>
                     </IconButton>
                     <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
                         Hrms
@@ -143,16 +143,16 @@ export default function Dashboard() {
                 open={open}>
                 <div className={classes.toolbarIcon}>
                     <IconButton onClick={handleDrawerClose}>
-                        <ChevronLeftIcon />
+                        <ChevronLeftIcon/>
                     </IconButton>
                 </div>
-                <Divider />
+                <Divider/>
                 <List>{mainListItems}</List>
-                <Divider />
+                <Divider/>
                 <List>{secondaryListItems}</List>
             </Drawer>
             <main className={classes.content}>
-                <div className={classes.appBarSpacer} />
+                <div className={classes.appBarSpacer}/>
                 <Container maxWidth="lg" className={classes.container}>
                     <Grid container spacing={3}>
                         <Grid item xs={12}>
